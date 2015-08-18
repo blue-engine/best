47dd53f553b9ed18f1b7b580a271e207f23a6d6a
Frank Scarfo <frank.scarfo@warbyparker.com>
2015-08-18 12:02:19 -0400

    Remove django secret from public repo

--- blueengine/settings.py
+++ blueengine/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jl+ebx)1+brr61+%2!2(wc+^@s+2@yc%yw5a0(szrqpsd-2l*e'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'TEST_LOCAL_SECRET_KEY_CHANGE_IN_PROD')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
